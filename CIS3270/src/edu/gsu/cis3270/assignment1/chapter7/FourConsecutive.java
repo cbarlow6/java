c2ff3db732988bfdf49a09f2995c2cfc82bfcbc0
cat barlow <cbarlow6@student.gsu.edu>
2016-10-06 20:46:18 -0400

    update four consecutive

--- CIS3270/src/edu/gsu/cis3270/assignment1/chapter7/FourConsecutive.java
+++ CIS3270/src/edu/gsu/cis3270/assignment1/chapter7/FourConsecutive.java
@@ -26,9 +26,46 @@ public class FourConsecutive {
 	}
 
 	public static boolean isConsecutiveFour(int [][] values){
-		boolean result = false;
+		//boolean result = false;
+		//check rows
+		for( int row = 0; row < values.length; row++)
+			for(int col = 0; col < values[row].length-3; col++)
+			if(values[row][col] == values[row][col+1] 
+				&& values[row][col] == values[row][col + 2] 
+				&& values[row][col] == values[row][col + 3]	)
+				return true;
 		
-		if(getCount(values) > 3)
+		//check columns
+		for(int col = 0; col < values[0].length; col++)
+			for( int row = 0; row < values.length - 3; row++)	
+				if(values[row][col] == values[row +1][col] 
+					&& values[row][col] == values[row+ 2][col] 
+					&& values[row][col] == values[row+ 3][col])
+					return true;
+		
+		//check major diagonal
+		for( int row = 0; row < values.length - 3; row++)
+			for(int col = 0; col < values[row].length - 3; col++)
+			if(values[row][col] == values[row + 1][col+1] 
+				&& values[row][col] == values[row + 2][col + 2] 
+				&& values[row][col] == values[row + 3][col + 3]	)
+				return true;
+		
+		// check minor diagonal
+		for( int row = 3; row < values.length; row++)
+			for(int col = 0; col < values[row].length - 3; col++)
+			if(values[row][col] == values[row - 1][col+1] 
+				&& values[row][col] == values[row - 2][col + 2] 
+				&& values[row][col] == values[row - 3][col + 3]	)
+				return true;
+		
+		return false;
+	}
+		
+		
+		
+		
+		/*if(getCount(values) > 3)
 			result = true;
 		
 		return result;
@@ -55,6 +92,6 @@ public class FourConsecutive {
 			}				
 			System.out.println(count);	
 			return count;
-		}
+		}*/
 		}
 
