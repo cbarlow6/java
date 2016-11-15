d284efdde53e5baceaa8447a9ae38f09969a2779
cat barlow <cbarlow6@student.gsu.edu>
2016-11-15 00:38:25 -0500

    update atm

--- CIS3270/src/edu/gsu/cis3270/assignment2/chapter10/TestATM.java
+++ CIS3270/src/edu/gsu/cis3270/assignment2/chapter10/TestATM.java
@@ -1,13 +1,34 @@
 package edu.gsu.cis3270.assignment2.chapter10;
-
+import java.util.Scanner;
 import java.util.Date;
 
 public class TestATM {
 	public static void main(String [] args){
 		
+		int[] accounts = new int[]{100, 100, 100, 100, 100, 100, 100, 100, 100, 100};
 		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter user id");
 		
+		int id = input.nextInt();
 		
+		while(id > 9 || id < 0){
+			System.out.println("Re-enter user id");
+			id = input.nextInt();
+		}
+		
+		if(id <= 9 && id >= 0){
+			
+			System.out.println("Main menu");
+			System.out.println("1: check balance");
+			System.out.println("2: withdraw");
+			System.out.println("3: deposit");
+			System.out.println("4: exit");
+			System.out.println("Enter a choice: ");
+		
+		}
+		
+	input.close();	
 	}
 
 }
@@ -19,12 +40,12 @@ class Account {
 	private Date dateCreated = new Date();
 	
 	//Constructors
-	public Account(){
+	Account(){
 			
 	}
-	public Account(int newId, double newBalance){
-		id = newId;
-		balance = newBalance;
+	public Account(int id, double balance){
+		this.id = id;
+		this.balance = balance;
 		
 		
 	}
@@ -46,16 +67,16 @@ class Account {
 		
 	}
 	// Setter Methods
-	public void setId(int newId){
-		id = newId;
+	public void setId(int id){
+		this.id = id;
 	}
 	
-	public void setBalance(double newBalance){
-		balance = newBalance;
+	public void setBalance(double balance){
+		this.balance = balance;
 	}
 	
-	public void setAnnualInterestRate(double newAnnualInterestRate){
-		annualInterestRate = newAnnualInterestRate;
+	public void setAnnualInterestRate(double annualInterestRate){
+		this.annualInterestRate = annualInterestRate;
 	}
 	
 	//Methods
